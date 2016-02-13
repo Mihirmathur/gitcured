a13067d50f4443ad373b42d10ef7fa88a35e6770
Mihirmathur <mihirmathur@g.ucla.edu>
2016-02-13 03:46:47 -0800

    added ids to input boxes

--- public/Header.js
+++ public/Header.js
@@ -1,9 +1,9 @@
 var Hello = React.createClass({  
   render: function() {
     return <div>
-    <input>
+    <input type="email" className="form-control" id="exampleInputEmail1" placeholder="Email">
     </input>
-    <input>
+    <input type="email" className="form-control" id="exampleInputEmail1" placeholder="password">
     </input>
     </div>;
 
