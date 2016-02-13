2780bef02cc99e8fc0a394d48668d33eee296803
Mihirmathur <mihirmathur@g.ucla.edu>
2016-02-13 04:00:40 -0800

    rearranged files

--- config/routes.js
+++ config/routes.js
@@ -3,9 +3,9 @@ var React = require('react');
 
 
 module.exports = function(app) {
-  // app.get('/', function(req, res) {
-  //   res.send({ok: true})
-  // })
+  app.get('/search', function(req, res) {
+    res.send('healthify_results.html')
+   })
   app.post('/login', function(req, res) {
     
   })
