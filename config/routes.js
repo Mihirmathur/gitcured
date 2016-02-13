7c73b1458ea5123760d3461fd404ced17b60e51b
Mihirmathur <mihirmathur@g.ucla.edu>
2016-02-13 04:38:45 -0800

    completed header module

--- config/routes.js
+++ config/routes.js
@@ -4,7 +4,7 @@ var React = require('react');
 
 module.exports = function(app) {
   app.get('/search', function(req, res) {
-    res.send('healthify_results.html')
+    res.send(healthify_results.html)
    })
   app.post('/login', function(req, res) {
     
--- public/Header.js
+++ public/Header.js
@@ -1,12 +1,27 @@
 var Hello = React.createClass({  
   render: function() {
     return <div>
-    <input type="email" className="form-control" id="exampleInputEmail1" placeholder="Email">
-    </input>
-    <input type="email" className="form-control" id="exampleInputEmail1" placeholder="password">
-    </input>
+    <nav className="navbar navbar-default navbar-fixed-top">
+	    <div className="container">
+	        <div className="navbar-header">
+	        </div>
+	     	<div className="col-md-6">
+	     	<h5 className="brand"> Health </h5>
+	     	</div>
+	    	<div className="col-md-3 top">
+				<input type="email" className="form-control" id="InputEmail" placeholder="Email">
+	    		</input>
+	    	</div>
+	    	<div className="col-md-3 top">
+	    	
+	    		<input type="email" className="form-control" id="InputPassword" placeholder="password">
+	    		</input>
+	    	
+	    		
+	    	</div>
+	    </div>
+    </nav>
     </div>;
-
   }
 });
 
