294901c28008e54cc3d161fa5f9860203e49b734
David Jimenez <davidjosuedjdj@gmail.com>
2016-02-13 14:28:39 -0800

    Added /user route

--- config/routes.js
+++ config/routes.js
@@ -7,7 +7,7 @@ var passport = require('passport');
 
 module.exports = function(app) {
   app.get('/search', function(req, res) {
-    // console.log("the session: ", req.session);
+    console.log("the session: ", req.session);
     if (req.session.passport.user) {
       res.sendfile('./public/healthify_results.html')
     } else {
@@ -31,7 +31,7 @@ module.exports = function(app) {
   })
   app.post('/question/create', function(req, res) {
     console.log("post request to /question/add ", req.body);
-    questions.create(req, res);
+    // questions.create(req, res);
   })
   app.post('/question/upvote', function(req, res) {
     console.log("post request to /question/upvote ", req.body);
@@ -40,6 +40,9 @@ module.exports = function(app) {
   app.post('/chat/post',function(req, res){
     console.log("chat request has been made", req.body)
     questions.chat(req, res);
+  })
+  app.get('/user', function(req, res) {
+    users.find(req, res);
   })
   app.get('/logout', function(req, res){
     req.logout();
